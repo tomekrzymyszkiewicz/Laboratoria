bf7b12e1dd419d01b92f372801dd55f26f58cbe1
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2019-11-07 13:02:58 +0100

    zadanie2

--- Lab3/Lab.cpp
+++ Lab3/Lab.cpp
@@ -39,7 +39,33 @@ void zadanie1(){
 		cout<<endl;
 	system("pause");
 }
-
+void zadanie2(){
+	system("CLS");
+	cout<<"-----------\n|Zadanie 3|\n-----------\n";
+	cout<<"Program drukujacy tabliczke mnozenia\nPodaj liczbe wierszy: ";
+	int wiersze, kolumny;
+	cin>>wiersze;
+	cout<<"Podaj liczbe kolumn: ";
+	cin>>kolumny;
+	printf("    | ");
+	for(int i = 1; i <= kolumny; i++){
+		printf(" %3d",i);
+	}
+	cout<<endl;
+	for(int i = 1; i <= 6+kolumny*4; i++){
+		printf("-");
+	}
+	cout<<endl;
+	for(int i = 1; i <= wiersze; i++){
+		printf("%3d | ", i);
+		for(int j = 1; j <= kolumny; j++){
+			printf(" %3d",i*j);
+		}	
+		cout<<endl;
+	}
+	
+	system("pause");
+}
 
 int main(){
 bool dzialaj = true;
@@ -49,7 +75,7 @@ bool dzialaj = true;
 	cout<<"Autor: Tomasz Rzymyszkiewicz \nGrupa: SR/P 15:15 \nTemat: Zadania - Laboratorium 3 \nData: 30.10.2019\n";
 	cout<<"\n-----------------\n|Wybierz zadanie|\n-----------------\n";
 	cout<<"1. Rysowanie choinki\n";
-	cout<<"2. \n";
+	cout<<"2. Tabliczka mnozenia\n";
 	cout<<"3. \n";
 	cout<<"4. \n";
 	cout<<"5. \n";
@@ -62,6 +88,9 @@ bool dzialaj = true;
 	{
 	case '1':
 		zadanie1();
+		break;
+	case '2':
+		zadanie2();
 		break;
 	
 	case 27: 
