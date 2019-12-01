5a52c0d2c965504588b8b84a07cfd82f48228d81
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2019-12-01 15:42:47 +0100

    Całe pierwsze

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -7,10 +7,11 @@ Data: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 #include <iostream>
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
 #define ROZMIAR 5
 using namespace std;
 float tablica[ROZMIAR];
-void wczytaj(){
+void wczytaj(){ //WCZYTANIE DANYCH
 	system("CLS");
 	printf("Wczytywanie danych do tablicy %d-elementowej\n",ROZMIAR);
 	for(int i = 0; i < ROZMIAR; i++){
@@ -20,7 +21,7 @@ void wczytaj(){
 	printf("Wczytano wszystkie wartosci do tablicy\n");
 	system("pause");
 }
-void wypisz(){
+void wypisz(){ //WYPISANIE DANYCH
 	system("CLS");
 	printf("Tab = [");
 	for(int i = 0; i < ROZMIAR; i++){
@@ -29,7 +30,8 @@ void wypisz(){
 	printf("]");
 	system("pause");
 }
-void zadanie1(){
+void zadanie1(){ //LISTA SPRAWDZEÑ Z ZADANIA 1
+	system("CLS");
 	int ileDodatnich = 0, ileUjemnych = 0;
 	float sumaDodatnich = 0, sumaUjemnych = 0;
 	for(int i = 0; i < ROZMIAR; i++){
@@ -42,11 +44,44 @@ void zadanie1(){
 			sumaUjemnych += tablica[i];
 		}
 	}
-	
+	float sredniaDodatnich = sumaDodatnich/ileDodatnich;
+	float sredniaUjemnych = sumaUjemnych/ileUjemnych;
+	printf("W tablicy jest:\n");
+	printf("-> %d liczb dodatnich\n",ileDodatnich);
+	printf("-> %d liczb ujemnych\n",ileUjemnych);
+	printf("-> suma liczb dodatnich wynosi %.2f\n",sumaDodatnich);
+	printf("-> suma liczb ujemnych wynosi %.2f\n",sumaUjemnych);
+	if(ileDodatnich > 0){
+		printf("-> srednia liczb dodatnich wynosi %.2f\n",sredniaDodatnich);
+	}
+	if(ileUjemnych > 0){
+		printf("-> srednia liczb ujemnych wynosi %.2f\n",sredniaUjemnych);
+	}
+	for(int i = 0; i < ROZMIAR; i++){
+		if(tablica[i]>tablica[i+1]){
+			break;
+		}
+		if(i = ROZMIAR-1){
+			printf("Elementy tablicy ustawione sa rosnaco\n");
+		}
+	}
+	for(int i = ROZMIAR-1; i > 0; i--){
+		if(tablica[i] > tablica[i-1]){
+			break;
+		}
+		if(i = 1){
+			printf("Elementy tablicy ustawione sa malejaco\n");
+		}
+	}
+	system("pause");
 }
 int main()
 {
+	bool dzialaj = true;
+	while(dzialaj){
 	wczytaj();
 	wypisz();
+	zadanie1();
+	}
 	return 0;
 }
