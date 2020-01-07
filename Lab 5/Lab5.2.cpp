93c2270c22a795802f33dbece4e74b943e02eded
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2020-01-07 08:34:30 +0100

    strcpy

--- Lab 5/Lab5.2.cpp
+++ Lab 5/Lab5.2.cpp
@@ -7,8 +7,20 @@ int moj_strlen(char tablica[]){
 	}
 	return licznik;
 }
+void moj_strcpy(char tab1[],char tab2[]){
+	int tab2_length = 0;
+	while(tab2[tab2_length] != '\0'){
+		tab2_length++;
+	}
+	int i = 0;
+	for(i = 0; i < tab2_length; i ++){
+		tab1[i] = tab2[i];
+	}
+	tab1[i] = '\0';
+}
 int main()
 {
+	/*
 	//TEST FUNCKJI moj_strlen
 	char tab1[] = "ala ma kota";
 	char tab2[10] = "ala";
@@ -18,6 +30,17 @@ int main()
 	for(int i = 0; i < moj_strlen(tab2); i++) printf("%c",tab2[i]);
 	printf("\nmoj_strlen = %d\n",moj_strlen(tab2));
 	//-----------------------
+	*/
+	/*
+	//TEST FUNCKJI moj_strlen
+	char tab3[] = "123hvhvhjvhvhvhvhvh45";
+	char tab4[] = "qwertvgvy";
+	printf("%s\n%s\n\n",tab3,tab4);
+	moj_strcpy(tab3,tab4);
+	printf("%s\n%s\n",tab3,tab4);
+	*/
+	
+	
 	
 	return 0;
 }
